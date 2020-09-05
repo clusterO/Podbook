d9603422a3f483903b1393ad3596b918fdd8fb7f
cluster0 <fourmou.m@gmail.com>
2020-09-05 20:06:21 +0100

    add troll function implemented

--- trollbook-functions/functions/index.js
+++ trollbook-functions/functions/index.js
@@ -2,6 +2,7 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 
 admin.initializeApp();
+
 const db = admin.firestore();
 
 exports.getTrolls = functions.https.onRequest((request, response) => {
@@ -17,4 +18,28 @@ exports.getTrolls = functions.https.onRequest((request, response) => {
       return response.json(trolls);
     })
     .catch((err) => console.error(err));
+});
+
+exports.addTroll = functions.https.onRequest((request, response) => {
+  if (request.method !== "POST") {
+    return response.status(400).json({ message: `Methode not allowed` });
+  }
+
+  let newTroll = {
+    userHandle: request.body.userHandle,
+    troll: request.body.troll,
+    createdAt: admin.firestore.Timestamp.fromDate(new Date()),
+  };
+
+  db.collection("trolls")
+    .add(newTroll)
+    .then((doc) => {
+      return response.json({
+        message: `document ${doc.id} created successfully`,
+      });
+    })
+    .catch((err) => {
+      response.status(500).json({ error: `something went south ${err}` });
+      console.error(err);
+    });
 });

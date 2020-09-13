6e54eb694f0eca950ff0eaaf3336bc082d0eb592
cluster0 <fourmou.m@gmail.com>
2020-09-13 01:09:33 +0100

    handling login

--- trollbook-client/src/pages/login.js
+++ trollbook-client/src/pages/login.js
@@ -1,11 +1,153 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { withStyles } from "@material-ui/core/styles";
+import PropTypes from "prop-types";
+import {
+  Grid,
+  TextField,
+  Button,
+  Typography,
+  CircularProgress,
+} from "@material-ui/core";
+import AppIcon from "../assets/images/troll.ico";
+import axios from "axios";
 
-export default class Login extends Component {
+const styles = {
+  form: {
+    textAlign: "center",
+  },
+  image: {
+    width: "50px",
+    height: "50px",
+  },
+  pageTitle: {
+    margin: "10px auto 10px auto",
+  },
+  textField: {},
+  button: {
+    margin: "20px auto 20px auto",
+    position: "relative",
+  },
+  customError: {
+    color: "red",
+    fontSize: "0.8rem",
+    marginTop: 10,
+  },
+  progress: {
+    position: "absolute",
+  },
+};
+
+class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+      loading: false,
+      errors: {},
+    };
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    this.setState({
+      loading: true,
+    });
+
+    const userData = {
+      email: this.state.email,
+      password: this.state.password,
+    };
+
+    axios
+      .post("/login", userData)
+      .then(result => {
+        console.log(result.data);
+        this.setState({
+          loading: false,
+        });
+        this.props.history.push("/");
+      })
+      .catch(err => {
+        this.setState({
+          errors: err.response.data,
+          loading: false,
+        });
+      });
+  };
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
   render() {
+    const { classes } = this.props;
+    const { errors, loading } = this.state;
+
     return (
-      <div>
-        <h1>Login Page</h1>
-      </div>
+      <Grid container className={classes.form}>
+        <Grid item sm />
+        <Grid item sm>
+          <img src={AppIcon} alt="troll" className={classes.image} />
+          <form noValidate onSubmit={this.handleSubmit}>
+            <TextField
+              id="email"
+              name="email"
+              type="email"
+              label="email"
+              className={classes.textField}
+              helperText={errors.email}
+              error={errors.email ? true : false}
+              value={this.state.email}
+              onChange={this.handleChange}
+              fullWidth
+            />
+            <TextField
+              id="password"
+              name="password"
+              type="password"
+              label="password"
+              className={classes.textField}
+              helperText={errors.password}
+              error={errors.password ? true : false}
+              value={this.state.password}
+              onChange={this.handleChange}
+              fullWidth
+            />
+            {errors.general && (
+              <Typography variant="body2" className={classes.customError}>
+                {errors.general}
+              </Typography>
+            )}
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              className={classes.button}
+              disabled={loading}
+            >
+              Login
+              {loading && (
+                <CircularProgress size={20} className={classes.progress} />
+              )}
+            </Button>
+          </form>
+          <small>
+            don't have an account? sign up <Link to="/signup">here</Link>
+          </small>
+        </Grid>
+        <Grid item sm />
+      </Grid>
     );
   }
 }
+
+Login.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Login);
